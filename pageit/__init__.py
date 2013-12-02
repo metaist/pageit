d92709f7d5425120391b0e125cc938fe1e2cbab5
The Metaist <metaist@metaist.com>
2013-12-01 23:32:53 -0500

    version bump

--- pageit/__init__.py
+++ pageit/__init__.py
@@ -9,7 +9,7 @@ __email__ = 'metaist@metaist.com'
 __license__ = 'MIT'
 __maintainer__ = 'The Metaist'
 __status__ = 'Prototype'
-__version__ = '0.2.2-alpha'
+__version__ = '0.2.2'
 __version_info__ = tuple(__version__.split('-')[0].split('.'))
 
 __epilog__ = '''Copyright 2013, Metaist.
